441efe095536b79a01e3f415ae7737e10e923405
Sneh Patel <snehpatel@MacBook-Pro-163.local>
2022-09-24 00:06:21 -0400

    Added a db model to save reports.

--- mandirInv/inventory/models.py
+++ mandirInv/inventory/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
 
 # Create your models here.
+
+
 class Area(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -11,9 +15,22 @@ class Area(models.Model):
 
 
 class Item(models.Model):
-    category = models.CharField(max_length=100)
+    uid = models.IntegerField()
+    description = models.CharField(max_length=200)
     quantity = models.IntegerField()
+    code = models.CharField(max_length=5)
     area = models.ForeignKey(Area, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.category
+        return self.code
+
+
+class Report(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    expected = models.IntegerField()
+    actual = models.IntegerField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.date)
