fe12e89d5403403a0fad69b94abdb67a763154b5
Sneh Patel <snehpatel@MacBook-Pro-163.local>
2023-07-08 19:08:37 -0400

    Added some venv stuff

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/snehpatel/PycharmProjects/mandirInv/venv"
+set -gx VIRTUAL_ENV "/Users/snehpatel/PycharmProjects/app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
